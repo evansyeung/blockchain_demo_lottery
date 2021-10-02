ff56f21b5005332626a3d569b703ddcb29703560
Evans Yeung <evansbyeung@gmail.com>
2021-10-02 18:37:35 -0400

    Implement lottery and true randomness

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,21 +3,43 @@
 pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-contract Lottery {
+// is Ownable means inheriting this class from OpenZeppelin
+// Also inherits VRFConsumerBase
+contract Lottery is VRFConsumerBase, Ownable {
   address payable[] public players;
-  uint256 usdEntryFee;
+  address payable recentWinner;
+  uint256 public randomness;
+  uint256 public usdEntryFee;
   AggregatorV3Interface internal ethUsdPriceFeed;
 
-  constructor(address _priceFeedAddress) public {
-    usdEntryFee = 50 * (10**18);  // fee in wei
-    ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+  // Values are represented by OPEN=0, CLOSED=1, CALCULATING_WINNER=2
+  enum LOTTERY_STATE {
+    OPEN,
+    CLOSED,
+    CALCULATING_WINNER
   }
 
-  function enter() public {
-    // $50 minimum
-    // require();
+  LOTTERY_STATE public lottery_state;
+  uint256 public fee;
+  bytes32 public keyHash;
 
+  // After public, we can add any additional contrustors from inherited contracts
+  constructor(address _priceFeedAddress, address _vrfCoordinator, address _link, uint256 _fee, bytes32 _keyHash) public VRFConsumerBase(_vrfCoordinator, _link) {
+    usdEntryFee = 50 * (10**18);  // fee in wei
+    ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+    lottery_state = LOTTERY_STATE.CLOSED;
+    fee = _fee;
+    keyHash = _keyHash;
+  }
+
+  function enter() public payable{
+    // Lottery has to be open to enter
+    require(lottery_state == LOTTERY_STATE.OPEN, "Lottery is not open!");
+    // $50 minimum
+    require(msg.value >= getEntranceFee(), "Not enough ETH!");
     players.push(msg.sender);
   }
 
@@ -32,11 +54,27 @@ contract Lottery {
     return costToEnter;
   }
 
-  function startLottery() public {
-
+  function startLottery() public onlyOwner {
+    require(lottery_state == LOTTERY_STATE.CLOSED, "Can't start a new lottery yet!");
+    lottery_state = LOTTERY_STATE.OPEN;
   }
 
-  function endLottery() public {
+  function endLottery() public onlyOwner {
+    lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+    bytes32 requestId = requestRandomness(keyHash, fee);
+  }
 
+  // Internal -> only allow our VRFCoordinator to call this function
+  // override -> Override VRFConsumerBase's fulfillRandomness function (which is meant to be overritten by us)
+  function fulfillRandomness(bytes32 _requestId, uint256 _randomness) internal override {
+    require(lottery_state == LOTTERY_STATE.CALCULATING_WINNER, "You aren't there yet!");
+    require(_randomness > 0, "random-not-found!");
+    uint256 indexOfWinner = _randomness % players.length;
+    recentWinner = players[indexOfWinner];
+    recentWinner.transfer(address(this).balance);
+    // Reset lottery
+    players = new address payable[](0);
+    lottery_state = LOTTERY_STATE.OPEN;
+    randomness = _randomness;
   }
 }
